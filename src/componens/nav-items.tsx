47362d609ee2a484f17569365ca14809a504dcfb
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-07-23 20:05:29 +0100

    Adiciona lógica para destacar itens de navegação com base na rota atual

--- src/componens/nav-items.tsx
+++ src/componens/nav-items.tsx
@@ -1,12 +1,31 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export function NavItems() {
+	const { pathname } = useLocation();
+	let subpage = pathname.split("/")?.[1];
+
+	function Linkness(type: string | null = null) {
+		if (subpage === "") {
+			subpage += "booking";
+		}
+
+		let classes = "text-base";
+
+		if (type === subpage) {
+			classes += " font-bold text-zinc-50";
+		} else {
+			classes += " font-normal text-zinc-50/50";
+		}
+
+		return classes;
+	}
+
 	return (
 		<nav className="flex items-center gap-4">
-			<Link to="/booking" className="font-bold text-base text-zinc-50">
+			<Link to="/booking" className={Linkness("booking")}>
 				Agendamentos
 			</Link>
-			<Link to="/doctors" className="font-normal text-base text-zinc-50/50">
+			<Link to="/doctors" className={Linkness("doctors")}>
 				Médicos
 			</Link>
 		</nav>
